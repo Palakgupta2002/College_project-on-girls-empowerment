3d38b0dac851d4db5960db6da6370f737a89f0d9
Palakgupta2002 <pg948303@gmail.com>
2023-05-18 00:12:16 +0530

    process

---
+++ public/profile.js
@@ -0,0 +1,21 @@
+
+
+
+// const fs = require("fs");
+// fs.readFile("./userData.json", "utf8", (err, jsonString) => {
+//   if (err) {
+//     console.log("File read failed:", err);
+//     return;
+//   }
+//   console.log("File data:", jsonString);
+//   console.log("hello");
+// });
+
+
+
+
+
+
+
+
+
